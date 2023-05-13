34a2fec87beceb9191629201008b41cd883bd1f1
ignacu1 <76869237+ignacu1@users.noreply.github.com>
2023-05-13 21:50:14 +0200

    Smoothing Movement Added

--- TimeCrime/Assets/Scripts/PlayerMovement.cs
+++ TimeCrime/Assets/Scripts/PlayerMovement.cs
@@ -4,7 +4,11 @@ public class PlayerMovement : MonoBehaviour
 {
     public float stopDistance = 0.1f;
     public float moveSpeed = 5f;
-    public bool canMove;
+    bool canMove;
+    public float stopVelocity = 0.1f;
+
+    [Range(0, 1)] public float rotationSmoothing;
+    [Range(0, 1)] public float movementSmoothing;
 
     private Rigidbody2D rb;
     public Camera cam;
@@ -28,18 +32,19 @@ public class PlayerMovement : MonoBehaviour
         mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
 
         //Tells animator, that player is moving
-        if (movement != 0 && canMove) playerAnim.SetBool("IsWalking", true);
+        if (rb.velocity.magnitude > stopVelocity && canMove) playerAnim.SetBool("IsWalking", true);
         else playerAnim.SetBool("IsWalking", false);
     }
 
     private void FixedUpdate()
     {
-        if(canMove)
-            rb.MovePosition(rb.position + (Vector2)transform.up * movement * moveSpeed * Time.fixedDeltaTime);
+        if (canMove)
+            rb.velocity = Vector2.Lerp((Vector2)transform.up * movement * moveSpeed * Time.fixedDeltaTime, rb.velocity, movementSmoothing);
+        else rb.velocity = Vector2.zero;
 
         Vector2 lookDir = mousePos - rb.position;
         float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
 
-        rb.rotation = angle;
+        rb.rotation = Mathf.LerpAngle(angle, rb.rotation, rotationSmoothing);
     }
 }
