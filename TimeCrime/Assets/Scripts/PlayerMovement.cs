93fc3a4545e2516ce5b7a02b52313cfa27a2b9ae
ignacu1 <76869237+ignacu1@users.noreply.github.com>
2023-05-13 20:45:51 +0200

    Revert "zepsulem nosz kurna"
    This reverts commit 2b583f682f94520d279c6a464d81063f8d464d55.

--- TimeCrime/Assets/Scripts/PlayerMovement.cs
+++ TimeCrime/Assets/Scripts/PlayerMovement.cs
@@ -11,6 +11,7 @@ public class PlayerMovement : MonoBehaviour
 
     private Rigidbody2D rb;
     public Camera cam;
+    public Animator playerAnim;
 
     Vector2 movement;
     Vector2 mousePos;
@@ -26,6 +27,10 @@ public class PlayerMovement : MonoBehaviour
         movement.y = Input.GetAxisRaw("Vertical");
 
         mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+
+        //Tells animator, that player is moving
+        if (movement.magnitude != 0) playerAnim.SetBool("IsWalking", true);
+        else playerAnim.SetBool("IsWalking", false);
     }
 
     private void FixedUpdate()
